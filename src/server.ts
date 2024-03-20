c9a44e0a0a803b9ed9c148349d91710429054d23
edutdutra <edutdutra@gmail.com>
2024-03-20 07:27:27 -0300

    initial commit - base project

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import fastify from 'fastify'
+import crypto from "node:crypto";
+import {knex} from "./database";
+import {env} from "./env";
+
+const app = fastify()
+
+
+app.get('/hello', async () => {
+    // const transaction = await knex('transactions').insert({
+    //     id: crypto.randomUUID(),
+    //     title: 'Transação de teste',
+    //     amount: 1000,
+    // }).returning('*')
+
+    const transaction = await knex('transactions').select('*')
+
+    return transaction
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
