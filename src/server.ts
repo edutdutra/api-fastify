be13dc94a329518c7601ade43499b57122650c0f
edutdutra <edutdutra@gmail.com>
2024-03-21 08:00:38 -0300

    add sessionId with cookies to validate user

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import fastify from 'fastify'
 import { env } from './env'
+import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
 
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(fastifyCookie)
 
 app.register(transactionsRoutes, {
   prefix: 'transactions',
